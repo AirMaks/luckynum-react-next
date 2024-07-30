cd45dd63018e87b36d49c1b0ae56e0292097cf0d
AirMaks <rm1010@yandex.ru>
2024-07-30 21:50:01 +0300

    rework

--- src/app/providers/providers.tsx
+++ src/app/providers/providers.tsx
@@ -4,18 +4,21 @@ import { StoreProvider } from "./StoreProvider";
 import { FontProvider } from "./FontProvider";
 import { ReactNode } from "react";
 import { ThemeProvider } from "next-themes";
+import { YandexMetrica } from "./YandexMetrika";
 
 interface Props {
     children: ReactNode;
 }
 export function Providers({ children }: Props) {
     return (
-        <StoreProvider>
-            <FontProvider>
-                <ThemeProvider attribute="class" defaultTheme="system">
-                    {children}
-                </ThemeProvider>
-            </FontProvider>
-        </StoreProvider>
+        <YandexMetrica>
+            <StoreProvider>
+                <FontProvider>
+                    <ThemeProvider attribute="class" defaultTheme="system">
+                        {children}
+                    </ThemeProvider>
+                </FontProvider>
+            </StoreProvider>
+        </YandexMetrica>
     );
 }
