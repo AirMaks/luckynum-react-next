898bf62a5ea142520f36b275f40b6ddd5cc716d9
AirMaks <rm1010@yandex.ru>
2024-08-05 21:57:24 +0300

    rework

---
+++ src/app/sitemap.ts
@@ -0,0 +1,24 @@
+import { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+    return [
+        {
+            url: "https://lucky-num.ru",
+            lastModified: new Date(),
+            changeFrequency: "monthly",
+            priority: 1
+        },
+        {
+            url: "https://lucky-num.ru/credit-calculator",
+            lastModified: new Date(),
+            changeFrequency: "monthly",
+            priority: 0.8
+        },
+        {
+            url: "https://lucky-num.ru/choose-from-list",
+            lastModified: new Date(),
+            changeFrequency: "monthly",
+            priority: 0.5
+        }
+    ];
+}
