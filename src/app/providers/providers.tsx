4a2d0e64d509ae604ced1be614f6bdadc619f5fa
AirMaks <rm1010@yandex.ru>
2024-08-20 17:15:20 +0300

    removed theme provider, font provider

--- src/app/providers/providers.tsx
+++ src/app/providers/providers.tsx
@@ -1,21 +1,9 @@
-"use client";
-
 import { StoreProvider } from "./StoreProvider";
-import { FontProvider } from "./FontProvider";
 import { ReactNode } from "react";
-import { ThemeProvider } from "next-themes";
 
 interface Props {
     children: ReactNode;
 }
 export function Providers({ children }: Props) {
-    return (
-        <StoreProvider>
-            <FontProvider>
-                <ThemeProvider attribute="class" defaultTheme="system">
-                    {children}
-                </ThemeProvider>
-            </FontProvider>
-        </StoreProvider>
-    );
+    return <StoreProvider>{children}</StoreProvider>;
 }
