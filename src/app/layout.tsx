93be2038a54b0cabe2b3887c029394f7ccdeb6d1
AirMaks <rm1010@yandex.ru>
2024-08-22 10:00:56 +0300

    overscroll none

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "styles/globals.scss";
 import { Jura } from "next/font/google";
 import { App } from "./app";
 import YandexMetrika from "./YandexMetrika";
+import cn from "classnames";
 
 const jura = Jura({ subsets: ["cyrillic", "latin"] });
 
@@ -13,14 +14,13 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-        // <html lang="ru" suppressHydrationWarning className={jura.className}>
-        <html lang="ru" className={jura.className}>
+        <html lang="ru" className={cn([jura.className, "overscroll-none"])}>
             <head>
                 <meta name="yandex-verification" content="b2606e0b53f2e31f" />
                 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=2" />
                 <link rel="shortcut icon" href="favicon.svg" type="image/svg+xml" />
             </head>
-            <body className="bg-lightbg text-black dark:bg-stone-800 dark:text-white">
+            <body className="bg-lightbg text-black dark:bg-stone-800 dark:text-white overscroll-none">
                 <App>{children}</App>
                 <YandexMetrika yid={88160252} clickmap={true} trackLinks={true} accurateTrackBounce={true} webvisor={true} />
             </body>
