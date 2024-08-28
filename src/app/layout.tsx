70e25508581147dd2397b099fcef418d93856e6f
AirMaks <rm1010@yandex.ru>
2024-08-28 15:07:35 +0300

    fixed favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
         <html lang="ru" className={cn([jura.className, ""])}>
             <head>
                 <meta name="yandex-verification" content="b2606e0b53f2e31f" />
-                <link rel="shortcut icon" href="favicon.svg" type="image/svg+xml" />
+                <link rel="shortcut icon" href="/favicon.svg" type="image/svg+xml" />
                 <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
                 <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
                 <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
