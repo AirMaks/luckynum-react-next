8967f7fdac2d932f4b0aeb289c51aaae7684393d
AirMaks <rm1010@yandex.ru>
2024-08-28 15:45:50 +0300

    added logo

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -3,6 +3,8 @@ import cls from "./Navbar.module.scss";
 import Burger from "shared/ui/Burger/Burger";
 import SettingsIcon from "shared/assets/icons/navbar/settings.svg";
 import CloseIcon from "shared/assets/icons/navbar/close.svg";
+import Link from "next/link";
+import Image from "next/image";
 
 interface NavbarProps {
     className?: string;
@@ -16,6 +18,9 @@ export const Navbar = ({ onToggleMenu, onToggleSettings, isOpenMenu, isOpenSetti
     return (
         <div className="z-[1001] dark:bg-stone-800 border-b dark:border-gray-600 h-[60px] sticky top-[0] w-full bg-white flex justify-between p-[20px] items-center">
             <div className="contents">
+                <Link href="/">
+                    <Image src="/img/logo.svg" alt="lucky num calculation" width={110} height={24} />
+                </Link>
                 {/* {isOpenSettings ? (
                     <CloseIcon
                         width={30}
