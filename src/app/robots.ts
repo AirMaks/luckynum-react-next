09a05326861358469c91c708a3adc11d14702a00
AirMaks <rm1010@yandex.ru>
2024-08-29 13:36:15 +0300

    removed disallow next static robots

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -5,7 +5,7 @@ export default function robots(): MetadataRoute.Robots {
         rules: {
             userAgent: "*",
             allow: "/",
-            disallow: ["/_next/static/"]
+            // disallow: ["/_next/static/"]
         },
         sitemap: "https://lucky-num.ru/sitemap.xml"
     };
