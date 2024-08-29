e4ca64b15ae0e4a1f1fb138fd445788db2bc3b49
AirMaks <rm1010@yandex.ru>
2024-08-29 16:54:00 +0300

    testing opengraph-image

--- src/app/metadata.ts
+++ src/app/metadata.ts
@@ -1,22 +1,20 @@
-export const metaData = ({ title, description, keywords, img }: any) => ({
-    title,
-    description,
-    keywords,
-    // metadataBase: new URL("https://lucky-num.ru"),
-    openGraph: {
-        title: title,
-        description: description,
-        type: "website",
-        ...(img
-            ? {
-                  images: [
-                      {
-                          url: img.src,
-                          width: img.width,
-                          height: img.height
-                      }
-                  ]
-              }
-            : {})
-    }
-});
+export const metaData = ({ title, description, keywords, img }: any) => {
+    return {
+        title,
+        description,
+        keywords,
+        // metadataBase: new URL("https://lucky-num.ru"),
+        openGraph: {
+            title: title,
+            description: description,
+            type: "website",
+            images: [
+                {
+                    url: img.src,
+                    width: img.width,
+                    height: img.height
+                }
+            ]
+        }
+    };
+};
