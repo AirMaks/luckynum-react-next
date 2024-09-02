488c1232ffac5cbb2b5e6c9dbfef736316683a69
AirMaks <rm1010@yandex.ru>
2024-09-02 15:57:28 +0300

    added percent calculator

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -4,7 +4,7 @@ export default function robots(): MetadataRoute.Robots {
     return {
         rules: {
             userAgent: "*",
-            allow: "/",
+            allow: "/"
             // disallow: ["/_next/static/"]
         },
         sitemap: "https://lucky-num.ru/sitemap.xml"
