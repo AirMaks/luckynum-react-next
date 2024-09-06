55fab2aa275e87189eee2a7d043d078913e9edf2
AirMaks <rm1010@yandex.ru>
2024-09-06 10:28:10 +0300

    added рся

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
     return (
         <>
             <RandomNumber animationSrc={{}} />;
-            <YandexAd />
+            {/* <YandexAd /> */}
         </>
     );
 }
