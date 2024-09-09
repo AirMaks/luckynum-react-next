56ccbb9dd14b12ec516a3dd0a76a13bc6210eea7
AirMaks <rm1010@yandex.ru>
2024-09-09 19:06:01 +0300

    next config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,6 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-    purge: ["./src/**/*.{js,ts,jsx,tsx}"],
     content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
     plugins: [],
     darkMode: "class"
