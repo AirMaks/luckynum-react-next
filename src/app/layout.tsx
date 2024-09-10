f003768e55aac2215ed80f1a0f1653b5d5812038
AirMaks <rm1010@yandex.ru>
2024-09-10 16:41:04 +0300

    removed zoom textarea

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,6 +30,7 @@ export default function RootLayout({
                 <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
                 <link rel="icon" type="image/png" sizes="192x192" href="/android-chrome-192x192.png" />
                 <link rel="icon" type="image/png" sizes="512x512" href="/android-chrome-512x512.png" />
+                <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
                 <Script
                     id="yaContextCb"
                     dangerouslySetInnerHTML={{
