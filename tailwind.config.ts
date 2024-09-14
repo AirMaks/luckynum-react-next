dd3b1861918d8a9cfd0109d9d545ebb567f83e2c
AirMaks <rm1010@yandex.ru>
2024-09-14 22:10:58 +0300

    added purge tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,6 +3,7 @@ import type { Config } from "tailwindcss";
 const config: Config = {
     content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
     plugins: [],
-    darkMode: "class"
+    darkMode: "class",
+    purge: ["./src/**/*.{js,ts,jsx,tsx,mdx}"]
 };
 export default config;
